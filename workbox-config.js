f7639cfe959ff20ac42e3746908845327098a087
Maxkehl <49717303+Maxkehl@users.noreply.github.com>
2020-12-14 08:45:23 +0100

    test

--- workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "globDirectory": "./",
-  "globPatterns": [
-    "**/*.{svg,png,html,json,mp3,xml,js,jpg}"
-  ],
-  "swDest": "sw.js",
-  "swSrc": "./src/src_sw.js",
-  maximumFileSizeToCacheInBytes: 20000000
-};
